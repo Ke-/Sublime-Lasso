21155775af47e8e84f59dfd01d2f3bfd04cb30c5
Ke <Ke-@users.noreply.github.com>
2019-03-07 10:05:00 +1300

    Update check_syntax.sh
    Removed absolute path from check syntax

--- Build/check_syntax.sh
+++ Build/check_syntax.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-result=$(/usr/bin/lassoc "$1" -n -o /tmp/lassoTMbundlecheck 2>&1)
+result=$(lassoc "$1" -n -o /tmp/lassoTMbundlecheck 2>&1)
 
 if [ "$result" == "" ]; then
     echo "No Problems Found"
