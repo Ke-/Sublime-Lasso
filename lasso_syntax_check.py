9ffb3178ebc9d7c194aee1350e52ef7ec279ed9a
Ke <Ke-@users.noreply.github.com>
2019-03-08 10:19:33 +1300

    Also removed path from lasso_syntax_check.py

--- lasso_syntax_check.py
+++ lasso_syntax_check.py
@@ -5,7 +5,7 @@ class LassoCheckSyntaxCommand(sublime_plugin.WindowCommand):
     def run(self, **kwargs):
         file_regex = "^Compiler error. ([^:])*: (.*?)line: (\\d+), col: (\\d+).*$"
 
-        cmd  = ['/usr/bin/lassoc', self.window.active_view().file_name(), '-n', '-o', '/dev/null']
+        cmd  = ['lassoc', self.window.active_view().file_name(), '-n', '-o', '/dev/null']
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         data = proc.communicate()[0].decode("utf-8") or "No Problems Found"
 
